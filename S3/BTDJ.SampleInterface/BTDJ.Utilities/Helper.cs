ca396651bdfa615ca904f54f292d0cbcf5e0daab
AzureBizTalkDJ <biztalkdj@gmail.com>
2018-12-05 00:43:42 +0530

    Session 3 Code
    Updated the Session 3

---
+++ S3/BTDJ.SampleInterface/BTDJ.Utilities/Helper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTDJ.Utilities
+{
+    [Serializable]
+    public class Helper
+    {
+        public string AgeGrp(int age)
+        {
+            string agegroup = "";
+            if (age < 15)
+            {
+                agegroup = "Level1";
+            }
+            else if (age >= 15 && age < 35)
+            {
+                agegroup = "Level2";
+
+            }
+            else if (age >= 35 && age < 55)
+            {
+                agegroup = "Level3";
+
+            }
+            else if (age >= 55 && age < 65)
+            {
+                agegroup = "Level4";
+
+            }
+            else
+            {
+                agegroup = "Level5";
+            }
+            return agegroup;
+        }
+    }
+}
